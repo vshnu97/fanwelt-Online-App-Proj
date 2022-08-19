76f23ee84e87d3ccd0872843cf147c6c2793d885
VISHNU K <vishnuky16@gmail.com>
2022-08-19 17:41:52 +0530

    done

--- lib/app/tournaments/view/screen_sub.dart
+++ lib/app/tournaments/view/screen_sub.dart
@@ -12,69 +12,72 @@ class ScreenSubTournament extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: PreferredSize(
-            preferredSize: Size(MediaQuery.of(context).size.width, 80.0),
-            child: const PageAppbar(title: 'Tournaments')),
-        body: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 15, left: 15, right: 15,bottom: 15),
-              height: 70,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                gradient: deffAppgradient,
-                border: Border.all(color: appBaseColor, width: 1.5),
-                color: offWhiteColor,
-              ),
-              child: ListTile(
-                  leading: const CircleAvatar(
-                    backgroundImage: AssetImage('assets/ipl.png'),
-                  ),
-                  title: Text('Indian Primier League', style: wTextSubStyle),
-                  subtitle: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        '54 matches',
-                        style: wTextSubStyleGrey,
-                      ),
-                      Text(
-                        'T-20',
-                        style: wTextSubStyleGrey,
-                      )
-                    ],
-                  )),
-            ),
-            Container(
-              child: Expanded(
-                child: ListView(
-                  shrinkWrap: true,
-                  physics: const BouncingScrollPhysics(),
-                  children: [
-                    Consumer<SubCardProv>(
-                      builder: (context, value, child) => ListView.separated(
-                          separatorBuilder: (context, index) {
-                            return kheight15;
-                          },
-                          physics: const BouncingScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount: 10,
-                          itemBuilder: (context, index2) {
-                            return InkWell(
-                                onTap: () {
-                                  value.changeColor(index2);
-                                },
-                                child:
-                                    tournamentSubCard(value.selectionIndex == index2,index2));
-                          }),
-                    ),
-                  ],
+    return Container(
+      color: kWhitecolor,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: PreferredSize(
+              preferredSize: Size(MediaQuery.of(context).size.width, 80.0),
+              child: const PageAppbar(title: 'Tournaments')),
+          body: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 15, left: 15, right: 15,bottom: 15),
+                height: 70,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  gradient: deffAppgradient,
+                  border: Border.all(color: appBaseColor, width: 1.5),
+                  color: offWhiteColor,
                 ),
+                child: ListTile(
+                    leading: const CircleAvatar(
+                      backgroundImage: AssetImage('assets/ipl.png'),
+                    ),
+                    title: Text('Indian Primier League', style: wTextSubStyle),
+                    subtitle: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          '54 matches',
+                          style: wTextSubStyleGrey,
+                        ),
+                        Text(
+                          'T-20',
+                          style: wTextSubStyleGrey,
+                        )
+                      ],
+                    )),
               ),
-            )
-          ],
+              Container(
+                child: Expanded(
+                  child: ListView(
+                    shrinkWrap: true,
+                    physics: const BouncingScrollPhysics(),
+                    children: [
+                      Consumer<SubCardProv>(
+                        builder: (context, value, child) => ListView.separated(
+                            separatorBuilder: (context, index) {
+                              return kheight15;
+                            },
+                            physics: const BouncingScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: 10,
+                            itemBuilder: (context, index2) {
+                              return InkWell(
+                                  onTap: () {
+                                    value.changeColor(index2);
+                                  },
+                                  child:
+                                      tournamentSubCard(value.selectionIndex == index2,index2));
+                            }),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
